f5f62bd2cae2bfd40226ff4c7fde85b78c335f55
MasKratos <profzumar@gmail.com>
2025-11-23 10:17:58 +0700

    Idle move logic

---
+++ Assets/EntityState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public abstract class EntityState
+{
+    protected Player player;
+    protected StateMachine stateMachine;
+    protected string animBoolName;
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public EntityState(Player player, StateMachine stateMachine, string animBoolName)
+    {
+        this.player =   player;
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+
+        anim = player.anim;
+        rb = player.rb; 
+    }
+
+    public virtual void Enter() 
+    {
+        player.anim.SetBool(animBoolName, true);
+    }
+    public virtual void Update() 
+    {
+       Debug.Log("Updating State: " + animBoolName);
+    }
+    public virtual void Exit()
+    { 
+       player.anim.SetBool(animBoolName, false);
+    }
+    
+}
